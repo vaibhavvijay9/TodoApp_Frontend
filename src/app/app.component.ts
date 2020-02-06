4592b5b4bcfda28412db5374888360d3c98de22b
Vaibhav Vijay <vaibhavvijay9@gmail.com>
2020-02-06 22:02:36 +0530

    u

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AppServiceService } from './services/app-service.service';
-import { Router, NavigationEnd } from '@angular/router'; // import Router and NavigationEnd
-// declare ga as a function to set and sent the events
-declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,18 +9,7 @@ export class AppComponent implements OnInit{
   
   isLoggedIn;
   
-  constructor(private myService: AppServiceService,public router: Router){
-    // subscribe to router events and send page views to Google Analytics
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        gtag('config', 'UA-157826759-1',
-          {
-            page_path: event.urlAfterRedirects
-          }
-        );
-      }
-    });
-  }
+  constructor(private myService: AppServiceService){}
 
   ngOnInit() {
     this.myService.getLoggedInFlagForNav().subscribe(data => {
